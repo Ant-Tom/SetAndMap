9b246a7363456f53ebac346eb84ea008a7f1d880
Anton <146194876+Ant-Tom@users.noreply.github.com>
2024-07-18 06:42:46 +0300

    Revert "home work set and map"
    This reverts commit 4023abb13194c11d29ddba5fd2b2ee428a56fde6.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        printOddNumbers(nums);
-    }
-
-    public static void printOddNumbers(List<Integer> numbers) {
-        for (Integer number : numbers) {
-            if (number % 2 != 0) {
-                System.out.println(number);
-            }
-        }
+        System.out.println("Hello world!");
     }
 }
